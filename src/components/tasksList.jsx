e79365da0df9f246e4d3b0441f58bc66ece488a0
sarabjeet singh <sarabsingh212@gmail.com>
2018-06-12 18:14:56 +0530

    add task

--- src/components/tasksList.jsx
+++ src/components/tasksList.jsx
@@ -4,17 +4,22 @@ class TaskList extends React.Component {
     state = {
         tasks: [
             {title: "Task 1", deadline:"Monday"},
-            {title: "Deliever the project", deadline:"Friday"},
-            {title: "Sprint Planning", deadline:"Thursday"},
-            {title: "Milestone", deadline:"Monday"},
-            {title: "Stylesheet Add", deadline:"Monday"}
-        ]
+            {title: "Deliever the project", deadline:"Friday"}
+        ],
+        addTask: false,
+        taskValue: ''
+    }
+    handleInput = (key, value) => {
+        this.setState(oldState => ({[key]:value}))
     }
     addTask = () => {
         const taskTitle = `Task ${this.state.tasks.length + 1}`;
         let tasks = this.state.tasks;
-        tasks.push(({title:taskTitle, deadline: "Tuesday"}));
-        this.setState(oldState => ({tasks: tasks}));
+        tasks.push(({title:this.state.taskValue, deadline: new Date().getDate()}));
+        this.setState(oldState => ({tasks, addTask: false, taskValue: ''}));
+    }
+    handleAdd = () => {
+        this.setState(oldState => ({addTask: true}))
     }
     render () {
         return (
@@ -31,6 +36,15 @@ class TaskList extends React.Component {
                         </div>
                     </li>
                     
+                    {this.state.addTask && <li>
+                        <div className="taskList__list taskList__Add-task">
+                            <div className="taskList__title">
+                                <form onSubmit={this.addTask}>
+                                    <input placeholder="title" value={this.state.taskValue} onChange={e => this.handleInput('taskValue', e.target.value)} />
+                                </form>
+                            </div>
+                        </div>
+                    </li>}
                     {this.state.tasks.map(task =>
                         <li key={task.title}>
                             <div className="taskList__list">
@@ -50,7 +64,7 @@ class TaskList extends React.Component {
                     )}
                     <li>
                         <div className="taskList__list taskList__list--button">
-                            <button onClick={this.addTask} className="taskList__add-button">Add New task</button>
+                            <button onClick={this.handleAdd} className="taskList__add-button">Add New task</button>
                         </div>
                     </li>
                 </ul>
